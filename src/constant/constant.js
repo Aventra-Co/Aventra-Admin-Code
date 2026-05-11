6c1dd222cdf5dd7a4c7c2f6f05d13245759977bd
Ahmed Safaa <ahmed@sz4h.com>
2026-05-11 19:45:47 +0300

    Placeholder

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -7,5 +7,5 @@ const DEV_MODE = (process.env.APP_ENV || 'production') !== 'production';
 
 export const API_URL    = `${SERVER_URL}/app/server/adminAPI`;
 export const IMAGE_PATH = DEV_MODE ? `${SERVER_URL}/uploads/` : `${SERVER_URL}/app/server/uploads/`;
-export const LOGO_URL   = `${SERVER_URL}/logo/logo.png`;
-export const placeholder = `${SERVER_URL}/uploads/placeholder.webp`;
+export const LOGO_URL   = DEV_MODE ? `${SERVER_URL}/logo/logo.png` : `${SERVER_URL}/app/server/logo/logo.png`;
+export const placeholder = DEV_MODE ? `${SERVER_URL}/uploads/placeholder.webp` : `${SERVER_URL}/app/server/uploads/Placeholder.webp`;
