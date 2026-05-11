6f50ecd87bad081984ea6ae3962703581afb7ed2
Ahmed Safaa <ahmed@sz4h.com>
2026-05-11 19:38:24 +0300

    Fix

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -3,8 +3,9 @@ export const BASE_TITLE = '';
 export const APP_PREFIX_PATH = '/app/central';
 
 const SERVER_URL = process.env.REACT_APP_SERVER_URL || 'https://aventra-co.com/app/server';
+const DEV_MODE = (process.env.APP_ENV || 'production') !== 'production';
 
 export const API_URL    = `${SERVER_URL}/app/server/adminAPI`;
-export const IMAGE_PATH = `${SERVER_URL}/uploads/`;
+export const IMAGE_PATH = DEV_MODE ? `${SERVER_URL}/uploads/` : `${SERVER_URL}/app/server/uploads/`;
 export const LOGO_URL   = `${SERVER_URL}/logo/logo.png`;
 export const placeholder = `${SERVER_URL}/uploads/placeholder.webp`;
